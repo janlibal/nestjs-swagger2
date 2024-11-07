88b33febad41141ab6cfbf7175dc34b805d69f9c
Jan Libal <libalj@seznam.cz>
2024-11-07 15:55:16 +0100

    Add employee module

---
+++ src/employee/employee.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+import { EmployeeController } from './employee.controller';
+import { EmployeeService } from './employee.service';
+
+
+
+
+@Module({
+  imports: [],
+  controllers: [EmployeeController],
+  providers: [EmployeeService],
+})
+export class EmployeeModule {}
